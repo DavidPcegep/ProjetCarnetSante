fa34d6f832065079b6ccc04b4511b63c1d3a7f46
Thomas Baudry <2132465@cegeprdl.ca>
2023-03-01 09:14:31 -0500

    Ajout page Ressource et NumeroUrgence

---
+++ src/page/ressource/infrastructure.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as Icon from 'react-bootstrap-icons';
+import "./infrastructure.css"
+import $ from "jquery";
+import logoMonCarnetSante from "../../assets/img/Logo/MonCarnetDeSanteTitre.png";
+
+
+function Infrastructure() {
+    return (
+      <div>
+        <div className="Content">
+          <a href="/ressource" className="position-absolute start-0 me-2 p-3 fs-1">
+              <i className="btn-exit bi bi-box-arrow-left"></i>
+          </a>
+          <div className="logoContent">
+              <img src={logoMonCarnetSante} alt="LogoMonCarnetSante" />
+          </div>
+          <hr></hr>
+            
+          </div>
+    </div>
+  );
+}
+
+export default Infrastructure;
